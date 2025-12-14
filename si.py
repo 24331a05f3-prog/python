174af06d0691b7eaf6be5f94c6ab8cc0a3f153f7
24331a05f3-prog <24331a05f3@mvgrce.edu.in>
2025-12-14 22:42:33 +0530

    Update si.py

--- si.py
+++ si.py
@@ -1,6 +1,6 @@
 principle=int(input("enter principle amount:"))
 rate=float(input("enter rate of interest(in%'):"))
 time=int(input("enter no of years:"))
-
 si=principle*time*rate/100
 print("simple interest for provided input is: ",si)
+
