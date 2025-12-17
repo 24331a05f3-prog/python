3b3d19ff9028e5d34735b959d946aa68e1c45f4c
24331a05f3-prog <24331a05f3@mvgrce.edu.in>
2025-12-17 21:11:51 +0530

    Update np.py

--- np.py
+++ np.py
@@ -1,4 +1,5 @@
-
-import numpy
-identity_matrix = numpy.eye(3)
-print(identity_matrix)
+import numpy 
+arr1 = [2, 3, 5, 7, 11] 
+arr2 = [1, 2, 3, 4, 5] 
+out = numpy.floor_divide(arr1, arr2) 
+print ("Output array : ", out)
